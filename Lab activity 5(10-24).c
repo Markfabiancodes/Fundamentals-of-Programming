dec019e40eab21c5d687a4f0f0c88bb56c1afbd2
Markfabiancodes <2025-1-01084@lpu.edu.ph>
2025-10-24 09:31:43 +0800

    Update Lab activity 5(10-24).c

--- Lab activity 5(10-24).c
+++ Lab activity 5(10-24).c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 int main() {
-   int num;
-   printf("🌟 Enter a number to reveal its cosmic table: ");
-   scanf("%d", &num);
-   printf("\n🌀 Multiplication Matrix of %d:\n", num);
-   for (int i = 1; i <= 10; i++) {
-       printf("✨ %d × %d = %d", num, i, num * i);
-       for (int j = 0; j < i; j++) {
-           printf("⭐");
-       }
-       printf("\n");
-   }
-   printf("\n🌠 Cosmic Calculation Complete! 🌠\n");
-   return 0;
+    int num;
+    printf("Enter a number to reveal its table: ");
+    scanf("%d", &num);
+
+    printf("\nMultiplication Table of %d:\n", num);
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d × %d = %d", num, i, num * i);
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\nCalculation Complete!\n");
+    return 0;
 }
