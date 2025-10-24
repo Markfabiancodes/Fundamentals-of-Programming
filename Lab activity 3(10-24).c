704eed4546bbe3fe648297d238a3cc9d046c28d2
Markfabiancodes <2025-1-01084@lpu.edu.ph>
2025-10-24 09:30:03 +0800

    Update Lab activity 3(10-24).c

--- Lab activity 3(10-24).c
+++ Lab activity 3(10-24).c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 int main() {
-  printf("✨ Odd Number Symphony ✨\n");
-  for (int i = 0; i <= 4; i++) {
-      int odd = (i << 1) + 1;
-      printf("🎭 %d", odd);
-      for (int j = 0; j <= odd; j += 2) {
-          printf("♡");
-      }
-      printf("\n");
-  }
-  printf("\n🌈 Odd Magic Complete! 🌈\n");
-  return 0;
+    printf("Odd Number Symphony\n");
+    
+    for (int i = 0; i <= 4; i++) {
+        int odd = (i << 1) + 1;
+        printf("%d", odd);
+        for (int j = 0; j <= odd; j += 2) {
+            printf(".");
+        }
+        printf("\n");
+    }
+
+    printf("\nOdd Magic Complete!\n");
+    return 0;
 }
