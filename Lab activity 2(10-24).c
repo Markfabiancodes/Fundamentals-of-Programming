e6f32344eab1d4854b474fb01efd30adf21d8b1e
Markfabiancodes <2025-1-01084@lpu.edu.ph>
2025-10-24 09:29:25 +0800

    Update Lab activity 2(10-24).c

--- Lab activity 2(10-24).c
+++ Lab activity 2(10-24).c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 int main() {
-   printf("✨ Even Number Symphony ✨\n");
+    printf("Even Number Symphony\n");
+    
     for (int i = 0; i <= 5; i++) {
-       int even = i << 1;
-       printf("🎭 %d", even);
-       for (int j = 0; j <= even; j += 2) {
-           printf("♡");
-       }
-       printf("\n");
-   }
-   printf("\n🌈 Even Magic Complete! 🌈\n");
-   return 0;
+        int even = i << 1;
+        printf("%d", even);
+        for (int j = 0; j <= even; j += 2) {
+            printf("~");
+        }
+        printf("\n");
+    }
+
+    printf("\nEven Magic Complete!\n");
+    return 0;
 }
-
-
